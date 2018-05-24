3098f6e5b025a0e8122cd9e87906242e6b11f2c0
Alyssa Kim <alyssa.kim@gmail.com>
2018-05-24 09:40:41 +0000

    automatically backed up by learn

---
+++ models/textanalyzer.rb
@@ -0,0 +1,14 @@
+class TextAnalyzer 
+  attr_reader :text
+
+  def initialize(text)
+    @text = text.downcase
+  end 
+
+  def count_of_words 
+    words = text.split(" ") 
+    words.count
+  end 
+
+  
+end
